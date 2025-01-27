6e8a6a3f18219d999e0867e94eedafc1b800d5c4
Alan Fernandes <61387305+allanfernds@users.noreply.github.com>
2025-01-27 19:36:23 -0300

    update client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,20 @@
-import { Database } from "@/types/supabase";
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
-export function supabaseClient() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+
+// Função para login com Discord
+export const loginWithDiscord = async () => {
+  const { error } = await supabaseClient.auth.signInWithOAuth({
+    provider: "discord",
+    options: {
+      redirectTo: "https://kchatroom.vercel.app/api/auth/v1/callback", // URL de redirecionamento
+    },
+  });
+
+  if (error) {
+    console.error("Erro ao autenticar com Discord:", error.message);
+  }
+};
